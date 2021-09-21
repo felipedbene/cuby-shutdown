be51692464fb73755de66793ca2e9445abd7cbc6
felipe de bene <felipe.debene@gmail.com>
2021-09-21 01:57:29 +0000

    First Commit

---
+++ cuby-app/app.py
@@ -0,0 +1,28 @@
+import json
+import requests
+import boto3
+import time
+
+def lambda_handler(event, context):
+    ssm = boto3.client('ssm')
+    urlApagado = ssm.get_parameter(Name='cuby-urlApagado', WithDecryption=True)
+    print('FIRST TRY: Starting Lambda')
+
+    while True :
+        try:
+            ip = requests.get(urlApagado["Parameter"]["Value"])
+            if ip.status_code == 200 :
+                print(ip.text)
+                resultJsonDict = json.loads(ip.text)
+                if resultJsonDict["status"] == "ok" :
+                    print("Shutdown command sent successfully")
+                    break
+                else :
+                    print("http ok but command fail, gonna try again")
+            else :
+                print("Status Code: " + str(ip.status_code) + ", Additional Data: " + ip.text)
+        except requests.RequestException as e:
+            time.sleep(1)
+            print("Failed http, retrying ... ")
+            continue
+    
---
+++ y
@@ -0,0 +1,10 @@
+version = 0.1
+[y]
+[y.deploy]
+[y.deploy.parameters]
+stack_name = "ac-auto"
+s3_bucket = "aws-sam-cli-managed-default-samclisourcebucket-1igy75j7zbyo6"
+s3_prefix = "ac-auto"
+region = "us-east-1"
+confirm_changeset = true
+capabilities = "CAPABILITY_IAM"
